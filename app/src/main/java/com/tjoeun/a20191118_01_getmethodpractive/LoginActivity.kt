f83e5a6c877a4683f4e9a7708e22a58b67df04ab
kiback2da <kiback2da@steconsulting.co.kr>
2019-11-18 20:14:06 +0900

    로그인 버튼을 누르면 Toast로 아이디 저장여부를 구별

--- app/src/main/java/com/tjoeun/a20191118_01_getmethodpractive/LoginActivity.kt
+++ app/src/main/java/com/tjoeun/a20191118_01_getmethodpractive/LoginActivity.kt
@@ -1,17 +1,29 @@
 package com.tjoeun.a20191118_01_getmethodpractive
 
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+        setupEvents()
+        setValues()
     }
 
     override fun setupEvents() {
+        loginBtnLogin.setOnClickListener {
+            if(loginChkBoxRememberID.isChecked){
+                Toast.makeText(mContext,"아이디를 저장해야 합니다.",Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(mContext,"아이디를 저장하지 않습니다.",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun setValues() {
+
     }
 }
