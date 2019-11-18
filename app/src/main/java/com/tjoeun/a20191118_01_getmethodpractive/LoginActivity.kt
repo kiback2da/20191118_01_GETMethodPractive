292c244fbcbddda996bf8cc3732f7b8ecb079639
kiback2da <kiback2da@steconsulting.co.kr>
2019-11-18 20:52:13 +0900

    만들어둔 ContextUtil 활용 예제

--- app/src/main/java/com/tjoeun/a20191118_01_getmethodpractive/LoginActivity.kt
+++ app/src/main/java/com/tjoeun/a20191118_01_getmethodpractive/LoginActivity.kt
@@ -1,7 +1,10 @@
 package com.tjoeun.a20191118_01_getmethodpractive
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.tjoeun.a20191118_01_getmethodpractive.util.ContextUtil
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity() {
@@ -9,6 +12,9 @@ class LoginActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        Log.d("로그 : ContextUtil","${}")
+
         setupEvents()
         setValues()
     }
@@ -16,7 +22,8 @@ class LoginActivity : BaseActivity() {
     override fun setupEvents() {
         loginBtnLogin.setOnClickListener {
             if(loginChkBoxRememberID.isChecked){
-                Toast.makeText(mContext,"아이디를 저장해야 합니다.",Toast.LENGTH_SHORT).show()
+                ContextUtil.setUserId(mContext,loginEdtID.text.toString())
+                Toast.makeText(mContext,"아이디를 저장했습니다..",Toast.LENGTH_SHORT).show()
             }else{
                 Toast.makeText(mContext,"아이디를 저장하지 않습니다.",Toast.LENGTH_SHORT).show()
             }
--- app/src/main/java/com/tjoeun/a20191118_01_getmethodpractive/util/ContextUtil.kt
+++ app/src/main/java/com/tjoeun/a20191118_01_getmethodpractive/util/ContextUtil.kt
@@ -10,6 +10,7 @@ class ContextUtil {
 
         //사용자의 id를 저장하는 항목 이름
         val USER_ID = "USER_ID"
+        val REMEMBER_ID = "REMEMBER_ID"
 
         //사용자 ID를 저장해주는 fun => setter
         fun setUserId(context: Context, userid:String){
@@ -20,10 +21,24 @@ class ContextUtil {
             pref.edit().putString(USER_ID,userid).apply()
         }
 
+        fun setRememberId(context: Context, rememberId:Boolean){
+            //메모장(파일이름 : ContextPreference)을 실제로 여는 동작
+            var pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+
+            //메모장에 내용을 작성하고 저장하는 부분
+            pref.edit().putBoolean(REMEMBER_ID,rememberId).apply()
+        }
+
         fun getUserId(context: Context) : String{
             var pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
 
             return pref.getString(USER_ID,"")!!
+        }
+
+        fun getRememberId(context: Context) : Boolean{
+            var pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+
+            return pref.getBoolean(REMEMBER_ID,false)
         }
     }
 
